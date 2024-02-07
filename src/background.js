fa0840fc91687e0a38ab041b5429e28497903c9b
stcksmsh <vukicevickosta@gmail.com>
2024-02-07 23:12:37 +0100

    Fixed issue where delimiter would "remove itself" after you typed it if it was the last character in the string Fixed issue where bits of text would "ranomly" be replaced with 'undefined' (hopefully it's fixed)

--- src/background.js
+++ src/background.js
@@ -1,36 +1,34 @@
-
 var DICT;
 var REVDICT = {};
 
-fetch('dictionary.json')
+fetch("dictionary.json")
     .then((response) => response.json())
     .then((json) => {
         DICT = json;
 
-        for(let key of Object.keys(DICT)){
+        for (let key of Object.keys(DICT)) {
             REVDICT[DICT[key]] = key;
         }
-    }
-);
+    });
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    if(request.action === 'get'){
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "get") {
         let response = DICT[request.word];
-        if(response === undefined){
-            response = '';
+        if (response === undefined) {
+            response = "";
         }
         sendResponse(response);
     }
-    if(request.action === 'reverse'){
+    if (request.action === "reverse") {
         let response = REVDICT[request.word];
-        if(response === undefined){
-            response = '';
+        if (response === undefined) {
+            response = "";
         }
         sendResponse(response);
     }
-    if(request.action === 'set'){
+    if (request.action === "set") {
         DICT[request.word] = request.replacement;
         REVDICT[request.replacement] = request.word;
-        sendResponse('success');
+        sendResponse("success");
     }
 });
--- src/content.js
+++ src/content.js
@@ -1,68 +1,96 @@
-const delim = ':';
+const delim = ":";
 
 /// used for delim, and replacing the word with the emoji
 async function keyReleased(event) {
     var textElement = event.srcElement;
-    if(event.key === delim){
-
+    if (event.key === delim) {
         var text;
 
-
-        if(textElement.tagName === 'TEXTAREA' || textElement.tagName === 'INPUT' || textElement.isContentEditable === false){
+        if (
+            textElement.tagName === "TEXTAREA" ||
+            textElement.tagName === "INPUT" ||
+            textElement.isContentEditable === false
+        ) {
             text = textElement.value;
-        }else{
+        } else {
             text = textElement.innerHTML;
         }
         /// the next 4 lines extract the text inside nested tags
-        text = text.split('>');
+        text = text.split(">");
         text = text[Math.floor(text.length / 2)];
-        text = text.split('<');
+        text = text.split("<");
         text = text[0];
-        
+
         /// get position of caret
         var caretPos = textElement.selectionStart;
-        if(caretPos === undefined){
+        if (caretPos === undefined) {
             var sequence = text.split(delim); /// get the sequence of words separated by the delimiter
-            var newText = '';
+            var newText = "";
             var replaced = false;
-            for(var i = 0; i < sequence.length; i++){
+            for (var i = 1; i < sequence.length - 1; i++) { /// start from 1, since the first element is before the first delimiter, and go to the second to last element, since the last element is after the last delimiter
+                if (sequence[i] === "" || sequence[i] === undefined) {
+                    continue;
+                }
                 var replacement = await checkDictionary(sequence[i]);
-                if(replacement != ''){
+                if (replacement != "" && replacement != undefined) {
                     newText += replacement;
                     replaced = true;
-                }else{
-                    if(!replaced && i > 0){
+                } else {
+                    if (!replaced && i > 0) {
                         newText += delim;
                     }
                     newText += sequence[i];
                     replaced = false;
                 }
             }
-            if(textElement.tagName === 'TEXTAREA' || textElement.tagName === 'INPUT' || textElement.isContentEditable === false){
-                textElement.value = newText;
-            }else{
-                document.execCommand('selectAll',false,null);
-                document.execCommand('insertHTML',false,newText);
+            if(sequence[sequence.length - 1] === "" && !replaced){
+                newText += delim;
             }
-        }else{
-
+            if (newText === undefined) {
+                return;
+            }
+            if (
+                textElement.tagName === "TEXTAREA" ||
+                textElement.tagName === "INPUT" ||
+                textElement.isContentEditable === false
+            ) {
+                textElement.value = newText;
+            } else {
+                document.execCommand("selectAll", false, null);
+                document.execCommand("insertHTML", false, newText);
+            }
+        } else {
             /// gets part of text before the next delimiter (including the delimiter)
             var sequence = text.slice(0, caretPos).split(delim);
-            if(sequence.length < 3){ /// there should be at least two delimiters
+            if (sequence.length < 3) {
+                /// there should be at least two delimiters
                 return;
             }
             sequence.pop(); /// remove the last element, which is the empty string after the last delimiter
             word = sequence.pop(); /// get the last word
-            var replacement = await checkDictionary(word);  
-            if(replacement != ''){
-                text = text.slice(0, caretPos - word.length - 2) + replacement + text.slice(caretPos);
-                if(textElement.tagName === 'TEXTAREA' || textElement.tagName === 'INPUT' || textElement.isContentEditable === false){
+            if (word === "" || word === undefined) {
+                return;
+            }
+            var replacement = await checkDictionary(word);
+            if (replacement != "" && replacement != undefined) {
+                text =
+                    text.slice(0, caretPos - word.length - 2) +
+                    replacement +
+                    text.slice(caretPos);
+                if (
+                    textElement.tagName === "TEXTAREA" ||
+                    textElement.tagName === "INPUT" ||
+                    textElement.isContentEditable === false
+                ) {
                     textElement.value = text;
-                }else{
-                    document.execCommand('selectAll',false,null);
-                    document.execCommand('insertHTML',false,text);
+                } else {
+                    document.execCommand("selectAll", false, null);
+                    document.execCommand("insertHTML", false, text);
                 }
-                setCaretPosition(textElement, caretPos - word.length + replacement.length);
+                setCaretPosition(
+                    textElement,
+                    caretPos - word.length + replacement.length
+                );
             }
         }
     }
@@ -71,80 +99,96 @@ async function keyReleased(event) {
 /// used for backspace, since we need the character which is being deleted
 async function keyPressed(event) {
     var textElement = event.srcElement;
-    
-    if(event.key === 'Backspace'){
+
+    if (event.key === "Backspace") {
         var caretPos = textElement.selectionStart;
-        if(caretPos === undefined){
-            return;/// if caret position is not available, we can't really do anything
+        if (caretPos === undefined) {
+            return; /// if caret position is not available, we can't really do anything
         }
 
         var text;
-        if(textElement.tagName === 'TEXTAREA' || textElement.tagName === 'INPUT' || textElement.isContentEditable === false){
+        if (
+            textElement.tagName === "TEXTAREA" ||
+            textElement.tagName === "INPUT" ||
+            textElement.isContentEditable === false
+        ) {
             text = textElement.value;
-        }else{
+        } else {
             text = textElement.innerHTML;
         }
-        text = text.split('>');
+        text = text.split(">");
         text = text[Math.floor(text.length / 2)];
-        text = text.split('<');
+        text = text.split("<");
         text = text[0];
-        
+
         /// get position of caret
         var codePointArray = Array.from(text);
-        var codeCharPos = 0;        
+        var codeCharPos = 0;
         var counter = 0;
-        for(var c of codePointArray){
-            if(counter === caretPos){
+        for (var c of codePointArray) {
+            if (counter === caretPos) {
                 break;
             }
             counter += c.length;
             codeCharPos++;
         }
-        var character = codePointArray[codeCharPos-1];
+        var character = codePointArray[codeCharPos - 1];
         var replacement = await checkReverseDictionary(character);
         Promise.resolve(replacement);
-        if(replacement != ''){
-            text = codePointArray.slice(0, codeCharPos-1).join('') + delim + replacement + codePointArray.slice(codeCharPos+1).join('');
-            if(textElement.tagName === 'TEXTAREA' || textElement.tagName === 'INPUT' || textElement.isContentEditable === false){
+        if (replacement != "") {
+            text =
+                codePointArray.slice(0, codeCharPos - 1).join("") +
+                delim +
+                replacement +
+                codePointArray.slice(codeCharPos + 1).join("");
+            if (
+                textElement.tagName === "TEXTAREA" ||
+                textElement.tagName === "INPUT" ||
+                textElement.isContentEditable === false
+            ) {
                 textElement.value = text;
-            }else{
-                document.execCommand('selectAll',false,null);
-                document.execCommand('insertHTML',false,text);
+            } else {
+                document.execCommand("selectAll", false, null);
+                document.execCommand("insertHTML", false, text);
             }
-            setCaretPosition(textElement, caretPos - (character.length - replacement.length - 1));
+            setCaretPosition(
+                textElement,
+                caretPos - (character.length - replacement.length - 1)
+            );
         }
     }
 }
 
 function setCaretPosition(elem, caretPos) {
-
-    if(elem != null) {
-        if(elem.createTextRange) {
+    if (elem != null) {
+        if (elem.createTextRange) {
             var range = elem.createTextRange();
-            range.move('character', caretPos);
+            range.move("character", caretPos);
             range.select();
-        }
-        else {
-            if(elem.selectionStart) {
+        } else {
+            if (elem.selectionStart) {
                 elem.focus();
                 elem.setSelectionRange(caretPos, caretPos);
-            }
-            else
-                elem.focus();
+            } else elem.focus();
         }
     }
 }
 
-async function checkDictionary(word){
-    var replacement = await chrome.runtime.sendMessage({"action": "get", "word": word});
+async function checkDictionary(word) {
+    var replacement = await chrome.runtime.sendMessage({
+        action: "get",
+        word: word,
+    });
     return replacement;
 }
 
-async function checkReverseDictionary(character){
-    var word = await chrome.runtime.sendMessage({"action": "reverse", "word": character});
+async function checkReverseDictionary(character) {
+    var word = await chrome.runtime.sendMessage({
+        action: "reverse",
+        word: character,
+    });
     return word;
 }
 
-
-document.addEventListener('keyup', keyReleased);
-document.addEventListener('keydown', keyPressed);
+document.addEventListener("keyup", keyReleased);
+document.addEventListener("keydown", keyPressed);
